5cc374a98ff0e558ffeb2675365c165179299dc9
John Jerald De Chavez <dechavezjohnjerald029@gmail.com>
2023-10-21 17:47:19 +0800

    Implement click events: record element that has been clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import mainHeroImage from './assets/component-01/Image-01.jpg';
 import mainHeroImageLarge from './assets/component-01/Image-01@2x.jpg';
 import secondaryHeroImage1 from './assets/component-01/Image-02.jpg';
@@ -5,15 +6,43 @@ import secondaryHeroImage1Large from './assets/component-01/Image-02@2x.jpg';
 import secondaryHeroImage2 from './assets/component-01/Image-03.jpg';
 import secondaryHeroImage2Large from './assets/component-01/Image-03@2x.jpg';
 import Preview from './components/Preview';
+import SlideOver from './components/SlideOver';
 
 import { posts } from './data';
 
 function App() {
+  const [open, setOpen] = useState(false);
+  const [result, setResult] = useState<Record<string, number>>({});
+
+  const recordClick = (blog: string) => {
+    console.log(blog);
+    setResult((prevResult) => {
+      const currentBlogCount = prevResult[blog] ?? 0;
+
+      return {
+        ...prevResult,
+        [blog]: currentBlogCount + 1,
+      };
+    });
+  };
+
   return (
     <div className='bg-brand'>
       <main className='mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-44 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:px-8'>
         <Hero />
-        <Blogs />
+        <Blogs onRecordClick={recordClick} />
+        <SlideOver open={open} onOpen={setOpen}>
+          <pre className='text-white inline-block'>
+            {JSON.stringify(result, undefined, 2)}
+          </pre>
+        </SlideOver>
+        <button
+          title='Result'
+          className='fixed z-90 bottom-10 right-8 bg-red-600 px-4 py-2 rounded-md flex justify-center items-center text-white text-xl hover:bg-red-700'
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          Result
+        </button>
       </main>
     </div>
   );
@@ -93,7 +122,11 @@ function Hero() {
   );
 }
 
-function Blogs() {
+type BlogProps = {
+  onRecordClick: (blog: string) => void;
+};
+
+function Blogs({ onRecordClick }: BlogProps) {
   return (
     <section>
       <h2 className='text-4xl text-center uppercase text-white font-medium'>
@@ -106,7 +139,7 @@ function Blogs() {
             key={post.id}
             className='flex max-w-xl flex-col items-start justify-between'
           >
-            <div className='w-full relative border-b-4 border-red-600'>
+            <div className='w-full border-b-4 border-red-600'>
               <img
                 src={post.image.default}
                 alt={post.description}
@@ -115,7 +148,7 @@ function Blogs() {
             </div>
             <div className='group relative'>
               <h3 className='mt-6 text-lg font-semibold leading-6 text-white group-hover:text-gray-400'>
-                <a href='#'>
+                <a href='#' onClick={() => onRecordClick(post.title)}>
                   <span className='absolute inset-0' />
                   {post.title}
                 </a>
@@ -124,7 +157,7 @@ function Blogs() {
                 {post.description}
               </p>
             </div>
-            <div className='relative mt-8 flex items-center gap-x-4'>
+            <div className='relative mt-8'>
               <div className='text-sm uppercase leading-6 border-b border-red-600'>
                 <p className='font-semibold text-white hover:text-gray-400'>
                   <a href='#'>
